f55a468f57acbc71d1737516ddbd2b54fc7d05c1
vaibhavpathak99 <pathakvaibhav380@gmail.com>
2022-12-15 17:36:52 +0530

    login

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+// import Form from './Form'
+import Home from './Home';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="*" element={<Login />}>
+        </Route>
+        <Route path="home" element={<Home />}></Route>
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
